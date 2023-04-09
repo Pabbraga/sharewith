9994f4194830336a289ef1008f28eeb41e89a54d
Pabbraga <pablofelix567@gmail.com>
2023-04-09 20:19:06 -0300

    correções

--- app/Models/user.php
+++ app/Models/user.php
@@ -8,7 +8,7 @@ class User extends Database{
     
     private $table = 'users';
 
-    public function find($columns, $conditions) {
+    public function find($columns = '*', $conditions) {
         $db = Database::getInstance();
     
         $data = $db->getList($this->table, $columns, $conditions);
